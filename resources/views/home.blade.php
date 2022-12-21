f32bd27b7e4bb4242cf9f99b3c71ebf596fe553a
SaverioCascelli <saverio.cascelli@gmail.com>
2022-12-21 14:57:27 +0100

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Hello Laravel</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <h2>from web.php {{ $string }}</h2>
+</body>
+
+</html>
